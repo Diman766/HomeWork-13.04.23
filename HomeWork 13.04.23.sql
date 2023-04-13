84edad390fa3ba5492baf638baec6c7cfbcc891a
unknown <Dmitiy_yar@mail.ru>
2023-04-13 11:23:48 +0300

    add task

---
+++ HomeWork 13.04.23.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `homework_13.04.23`.mobile_phones;
+SELECT manufacturer, price FROM `homework_13.04.23`.mobile_phones
+WHERE product_count >2;
+SELECT * FROM `homework_13.04.23`.mobile_phones
+WHERE manufacturer = 'Samsung';
